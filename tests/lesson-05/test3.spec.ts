504cbb004ab30b5a46aacc79209910cc526a903c
Kim Anh <kimanh81295@gmail.com>
2026-05-22 09:39:51 +0700

    fix: Update comment by mentor lesson 05

--- tests/lesson-05/test3.spec.ts
+++ tests/lesson-05/test3.spec.ts
@@ -8,23 +8,25 @@ test('Basic Action', async ({ page }) => {
     await test.step("Click on Product page", async () => {
         await page.locator("//a[text()='Bài học 3: Todo page']").click();
     });
-    //Add task
-    for (let i = 1; i <= 100; i++) {
-        await page.locator("//input[@id='new-task']").fill(`Todo${i}`);
-        await test.step("Click button Add task", async () => {
-            await page.locator("//button[@id='add-task']").click();
-        });
-    };
-    //Delete task có số lẻ
-    await test.step("Delete task có số lẻ", async () => {
-        page.on("dialog", async dialog => {
-            await dialog.accept();
-        });
 
+    //Add task
+    await test.step("Thêm mới 100 todo item", async () => {
         for (let i = 1; i <= 100; i++) {
-            if (i % 2 !== 0) {
-                await page.click(`//button[@id='todo${i}-delete']`);
+            await page.locator("//input[@id='new-task']").fill(`Todo ${i}`);
+            await page.locator("//button[@id='add-task']").click();
+        };
+
+        //Delete task có số lẻ
+        await test.step("Delete task có số lẻ", async () => {
+            page.on("dialog", async dialog => {
+                await dialog.accept();
+            });
+
+            for (let i = 1; i <= 100; i++) {
+                if (i % 2 !== 0) {
+                    await page.click(`//button[@id='todo-${i}-delete']`);
+                }
             }
-        }
+        });
     });
 });
